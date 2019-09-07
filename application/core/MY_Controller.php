174148ac2ff3107809558712b58d1f5b5f467e3a
Bayu Setiawan <bayu.tuodeschawk@gmail.com>
2019-09-07 22:58:06 +0700

    IDK

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Created by PhpStorm.
+ * User: Bayu Setiawan (bayu.tuodeschawk@gmail.com)
+ * Date: 05/09/2019
+ * Time: 19:23
+ * @property CI_Output output
+ * @property CI_Input input
+ * @property CI_Loader load
+ */
+class MY_Controller extends CI_Controller{}
+
+/*	ADDING ALL CLASS FILES */
+require_once __DIR__.DIRECTORY_SEPARATOR.'Graphql_Controller.php';
